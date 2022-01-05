e3fbb70c70eec03cc6c6a3e5335a0b237425a230
Rishabh Kumar Bakshi <37394382+rishabhkbakshi@users.noreply.github.com>
2022-01-05 12:22:55 +0530

    Add files via upload

---
+++ Visual Studio 2010/Projects/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 12;
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write(" *");
+                }
+                Console.WriteLine("");
+            }
+            Console.ReadLine();
+        }
+    }
+}
