865aeeac1af15411f8cb6f69a20998da86e0c2ea
Rishabh Kumar Bakshi <37394382+rishabhkbakshi@users.noreply.github.com>
2022-01-05 15:40:24 +0530

    Add files via upload

---
+++ Visual Studio 2008/Projects/User interface/Business Logic/BLL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+
+namespace Business_Logic
+{
+    public  class BLL
+    {
+        public static string Insert(Business_Object.BOL InsertD)
+        {
+            Data_Access.DAL DataAccess = new Data_Access.DAL();
+            return DataAccess.Data(InsertD);
+        }
+
+        public static DataTable Select()
+        {
+            return Data_Access.DAL.Datagrid();
+        }
+    }
+}
---
+++ Visual Studio 2008/Projects/User interface/Data Access/DAL.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Web;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+//using User interface
+
+namespace Data_Access
+{
+    public class DAL
+    {
+        public string Data(Business_Object.BOL InsertO)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection("Data Source=192.168.9.204; User Id=sa; Password=sqlserver2005; Database=rishabh");
+                SqlCommand cmd = new SqlCommand("usp_User_Insert", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                con.Open();
+                cmd.Parameters.AddWithValue("@Username", InsertO.Username);
+                cmd.Parameters.AddWithValue("@Password", InsertO.Password);
+                cmd.Parameters.AddWithValue("@Firstname", InsertO.Firstname);
+                cmd.Parameters.AddWithValue("@Lastname", InsertO.Lastname);
+                cmd.Parameters.AddWithValue("@Email", InsertO.Email);
+                cmd.Parameters.AddWithValue("@PhoneNo", InsertO.PhoneNo);
+                cmd.Parameters.AddWithValue("@Location", InsertO.Location);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                string str = "";
+                return str;
+                
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public static DataTable Datagrid()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection("Data Source=192.168.9.204; User Id=sa; Password=sqlserver2005; Database=rishabh");
+                SqlCommand cmd = new SqlCommand("usp_User_Select", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                con.Open();
+                DataTable dt = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+                con.Close();
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
